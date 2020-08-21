7511dc8219d2d5a16e5ce8463cf33344bd1415b7
wesley <zerofire.li@qq.com>
2020-08-21 18:42:50 +0800

    Initial commit

---
+++ src/main/java/com/blog/bolgblog/BolgBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.bolgblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolgBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/bolgblog/BolgBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.bolgblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolgBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
