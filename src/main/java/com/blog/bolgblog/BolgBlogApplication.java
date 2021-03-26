496bc07c1e87334036fb6ebad56aa916dfb6c032
Jaon-li <17717561090li>
2021-03-26 22:47:08 +0800

    nacos提交

--- src/main/java/com/blog/bolgblog/BolgBlogApplication.java
+++ src/main/java/com/blog/bolgblog/BolgBlogApplication.java
@@ -1,9 +1,13 @@
 package com.blog.bolgblog;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.blog.bloguser.mapper")
+@NacosPropertySource(dataId = "blog-service.yaml", autoRefreshed = true)
 public class BolgBlogApplication {
 
     public static void main(String[] args) {
